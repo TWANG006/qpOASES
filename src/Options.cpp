b001b593b46221e86b6e0caef9419ae973852fc9
Tianyi <tianyiwang666@gmail.com>
2022-07-07 09:17:53 -0400

    reverted

--- src/Options.cpp
+++ src/Options.cpp
@@ -91,7 +91,7 @@ Options& Options::operator=( const Options& rhs )
  */
 returnValue Options::setToDefault( )
 {
-	printLevel = PL_NONE;
+	printLevel = PL_MEDIUM;
 	#ifdef __DEBUG__
 	printLevel = PL_HIGH;
 	#endif
